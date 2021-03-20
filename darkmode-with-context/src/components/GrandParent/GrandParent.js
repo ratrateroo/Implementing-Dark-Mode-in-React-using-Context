4d755e9a34ff74f99f2d42916dbd5a70c0897c13
Mark <ratrateroo@gmail.com>
2021-03-20 21:24:39 +0800

    added grand parent component

---
+++ darkmode-with-context/src/components/GrandParent/GrandParent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '../Button/Button';
+const GrandParent = () => {
+	return (
+		<div>
+			<h3>Grand Parent</h3>
+			<Button />
+		</div>
+	);
+};
+
+export default GrandParent;
