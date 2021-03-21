cf3fd6c06a3522404cdbbdfbf0c01906627568e1
Mark <ratrateroo@gmail.com>
2021-03-21 23:27:52 +0800

    added parent component

---
+++ darkmode-with-context/src/components/Parent/Parent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '../Button/Button';
+const Parent = () => {
+	return (
+		<div>
+			<h3>Parent</h3>
+			<Button />
+		</div>
+	);
+};
+
+export default Parent;
