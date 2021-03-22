7efe2699ef895615f25e9c5224f3238e4b7a8879
Mark <ratrateroo@gmail.com>
2021-03-22 21:03:29 +0800

    added child component

---
+++ darkmode-with-context/src/components/Child/Child.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Button from '../Button/Button';
+import Parent from '../Parent/Parent';
+const Child = () => {
+	<div>
+		<Parent />
+		<Button />
+	</div>;
+};
+
+export default Child;
--- darkmode-with-context/src/components/GrandParent/GrandParent.js
+++ darkmode-with-context/src/components/GrandParent/GrandParent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import Button from '../Button/Button';
+import Parent from '../Parent/Parent';
+
 const GrandParent = () => {
 	return (
 		<div>
 			<h3>Grand Parent</h3>
+			<Parent />
 			<Button />
 		</div>
 	);
