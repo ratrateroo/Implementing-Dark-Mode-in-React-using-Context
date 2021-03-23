b899a3222e365238afa261c2d3a67996314bbb6a
Mark <ratrateroo@gmail.com>
2021-03-23 21:57:54 +0800

    added the baby component

---
+++ darkmode-with-context/src/components/Baby/Baby.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from '../Button/Button';
+const Baby = () => {
+	return (
+		<div>
+			<h3>Baby</h3>
+			<Button />
+		</div>
+	);
+};
+
+export default Baby;
